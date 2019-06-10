bfe916f59f2b7548e360939b102a926ec0017d13
Tony Hung <tbass134@gmail.com>
2019-06-10 07:02:28 -0400

    updated constants removed saving audio to gcp added NexmoClient class cleanup AudioProcessor class using model with 1 feature setting id to conversation uuid

--- websocket-demo.py
+++ websocket-demo.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from __future__ import absolute_import, print_function
-
+import wave
+import datetime
+import argparse
 import io
 import logging
 import os
@@ -10,8 +12,7 @@ import time
 from logging import debug, info
 import uuid
 import cgi
-import nexmo
-
+import audioop
 import requests
 import tornado.ioloop
 import tornado.websocket
@@ -21,39 +22,42 @@ import tornado.web
 import webrtcvad
 from tornado.web import url
 import json
-
 from base64 import b64decode
-
-#Only used for record function
-import datetime
-import wave
-
-import numpy as np
-from scipy.io import wavfile
-import librosa
+import nexmo
+import collections
 import pickle
-from google.cloud import storage
+import librosa
+import numpy as np
+
 
 from dotenv import load_dotenv
-from sklearn.ensemble import RandomForestClassifier
-
 load_dotenv()
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# Only used for record function
 
 logging.captureWarnings(True)
-
-
-CLIP_MIN_MS = 150  # ms - the minimum audio clip that will be used
-MAX_LENGTH = 10000  # Max length of a sound clip for processing in ms
-
 
 # Constants:
 MS_PER_FRAME = 20  # Duration of a frame in ms
 RATE = 16000
-SILENCE = 2  # How many continuous frames of silence determine the end of a phrase
-
+SILENCE = 15 # How many continuous frames of silence determine the end of a phrase
+CLIP_MIN_MS = 200  # ms - the minimum audio clip that will be used
+MAX_LENGTH = 4000  # Max length of a sound clip for processing in ms
+VAD_SENSITIVITY = 3
 CLIP_MIN_FRAMES = CLIP_MIN_MS // MS_PER_FRAME
+
+# Global variables
+conns = {}
+conversation_uuids = collections.defaultdict(list)
+uuids = []
+nexmo_client = None
+loaded_model = pickle.load(open("models/GaussianNB-20190130T1233.pkl", "rb"))
+print(loaded_model)
+
+
+# Environment Variables, these are set in .env locally
+HOSTNAME = os.getenv("HOSTNAME")
+PORT = os.getenv("PORT")
 
 MY_LVN = os.getenv("MY_LVN")
 APP_ID = os.getenv("APP_ID")
@@ -70,22 +74,6 @@ def _get_private_key():
     return private_key
 
 PRIVATE_KEY = _get_private_key()
-if PROJECT_ID and CLOUD_STORAGE_BUCKET:
-    storage_client = storage.Client(PROJECT_ID)
-    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
-
-# Global variables
-conns = {}
-clients = []
-conversation_uuids = dict()
-uuids = []
-
-loaded_model = pickle.load(open("models/KNeighborsClassifier-20190508T1842.pkl", "rb"))
-print(loaded_model)
-client = nexmo.Client(application_id=APP_ID, private_key=PRIVATE_KEY)
-print(client)
-print(APP_ID)
-print(PRIVATE_KEY)
 
 class BufferedPipe(object):
     def __init__(self, max_frames, sink):
@@ -116,67 +104,65 @@ class BufferedPipe(object):
         self.count = 0
         self.payload = b''
 
+class NexmoClient(object):
+    def __init__(self):
+        self.client = nexmo.Client(application_id=APP_ID, private_key=PRIVATE_KEY)
+    def hangup(self,conversation_uuid):
+        for uuid in conversation_uuids[conversation_uuid]:
+            try:
+                response = self.client.update_call(uuid, action='hangup')
+                print("hangup uuid {} response: {}".format(uuid, response))
+            except Exception as e:
+                print("Hangup error",e)
+    def speak(self, conversation_uuid):
+         for uuid in conversation_uuids[conversation_uuid]:
+            try:
+                response = self.client.send_speech(uuid, text='Answering Machine Detected')
+                print("response", response)
+            except Exception as e:
+                print(e)
+                pass
+
 class AudioProcessor(object):
-    def __init__(self, path, client):
+    def __init__(self, path, conversation_uuid):
         self._path = path
-        self.client = client
+        self.conversation_uuid = conversation_uuid
+
     def process(self, count, payload, id):
-        if count > CLIP_MIN_FRAMES:  # If the buffer is less than CLIP_MIN_MS, ignore it
-            fn = "{}rec-{}-{}.wav".format('', id, datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
+        if count > CLIP_MIN_FRAMES :  # If the buffer is less than CLIP_MIN_MS, ignore it
+            print("record clip")
+            fn = "rec-{}-{}.wav".format(id,datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
             output = wave.open(fn, 'wb')
-            output.setparams((1, 2, RATE, 0, 'NONE', 'not compressed'))
+            output.setparams(
+                (1, 2, RATE, 0, 'NONE', 'not compressed'))
             output.writeframes(payload)
             output.close()
-            debug('File written {}'.format(fn))
-            self.process_file(fn)
-            info('Processing {} frames for {}'.format(str(count), id))
-            self.removeFile(fn)
-        else:
-            info('Discarding {} frames'.format(str(count)))
-    def process_file(self, wav_file):
-        if loaded_model != None:
-            print("load file {}".format(wav_file))
-            X, sample_rate = librosa.load(wav_file, res_type='kaiser_fast')
-            X = librosa.resample(X, sample_rate, 16000)
-
-            duration = librosa.get_duration(X, sample_rate)
-            stft = np.abs(librosa.stft(X))
-            mfccs_40 = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-            mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate,n_mels=128,fmax=8000).T,axis=0)
-            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
-            sr=sample_rate).T,axis=0)
-
-            a = [mfccs_40, chroma, mel, contrast, tonnetz]
-
-            total_len = 0
-            for f in a:
-                total_len += f.shape[0]
-
-            features = np.vstack([np.empty((0,total_len)),np.hstack(a)])
-
-            prediction = loaded_model.predict(features)
-            # prediction = loaded_model.predict([mfccs_40])
+            prediction = self.predict_from_file(fn)
             print("prediction",prediction)
+            self.remove_file(fn)
 
             if prediction[0] == 0:
-                beep_captured = True
-                print("send_speech",uuids)
-                for id in uuids:
-                    response = self.client.send_speech(id, text='Answering Machine Detected')
-                    print("send_speech response",response)
-                time.sleep(4)
-                for id in uuids:
-                    try:
-                        self.client.update_call(id, action='hangup')
-                    except:
-                        pass
-
+                nexmo_client.speak(self.conversation_uuid)
+                time.sleep(2)
+                nexmo_client.hangup(self.conversation_uuid)
         else:
-            print("model not loaded")
-    def removeFile(self, wav_file):
-         os.remove(wav_file)
+            info('Discarding {} frames'.format(str(count)))
+
+    def predict_from_file(self, wav_file):
+        if loaded_model != None:
+            print("load file {}".format(wav_file))
+            start = time.time()
+            X, sample_rate = librosa.load(wav_file, res_type='kaiser_fast')
+            mfccs_40 = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+            prediction = loaded_model.predict([mfccs_40])
+            end = time.time()
+            print ("time {}".format(end - start))
+            return  prediction
+
+    def remove_file(self, wav_file):
+        os.remove(wav_file)
+
+
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def initialize(self):
@@ -184,87 +170,71 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.frame_buffer = None
         # Setup the Voice Activity Detector
         self.tick = None
-        self.id = uuid.uuid4().hex
+        self.id = None#uuid.uuid4().hex
         self.vad = webrtcvad.Vad()
-          # Level of sensitivity
+        # Level of sensitivity
+
+        self.vad.set_mode(VAD_SENSITIVITY)
+
         self.processor = None
         self.path = None
+        self.nexmo_client = NexmoClient()
+
         conns[self.id] = self
+
     def open(self, path):
         info("client connected")
-        clients.append(self)
         debug(self.request.uri)
         self.path = self.request.uri
         self.tick = 0
+
     def on_message(self, message):
         # Check if message is Binary or Text
         if type(message) != str:
             if self.vad.is_speech(message, RATE):
-                debug ("SPEECH from {}".format(self.id))
+                debug("SPEECH from {}".format(self.id))
                 self.tick = SILENCE
                 self.frame_buffer.append(message, self.id)
             else:
                 debug("Silence from {} TICK: {}".format(self.id, self.tick))
                 self.tick -= 1
                 if self.tick == 0:
-                    self.frame_buffer.process(self.id)  # Force processing and clearing of the buffer
+                    # Force processing and clearing of the buffer
+                    self.frame_buffer.process(self.id)
         else:
+            info(message)
             # Here we should be extracting the meta data that was sent and attaching it to the connection object
             data = json.loads(message)
-            print("on_message",data)
+
             if data.get('content-type'):
-                uuid = data.get('uuid')
-                self.vad.set_mode(3)
-                self.processor = AudioProcessor(self.path, client).process
+                conversation_uuid = data.get('conversation_uuid') #change to use
+                self.id = conversation_uuid
+                self.processor = AudioProcessor(
+                    self.path, conversation_uuid).process
                 self.frame_buffer = BufferedPipe(MAX_LENGTH // MS_PER_FRAME, self.processor)
                 self.write_message('ok')
+
     def on_close(self):
-        print("close")
         # Remove the connection from the list of connections
         del conns[self.id]
-        clients.remove(self)
-        info("client disconnected")
-
-
-class PingHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        self.write('ok')
-        self.set_header("Content-Type", 'text/plain')
-        self.finish()
+        print("client disconnected")
 
 class EventHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def post(self):
         data = json.loads(self.request.body)
         if data["status"] == "answered":
-            print("event:", self.request.body)
+            print(data)
 
-        try:
-            if data["status"] == "answered":
-                uuid = data["uuid"]
-                uuids.append(uuid)
-                conversation_uuid = data["conversation_uuid"]
-                conversation_uuids[conversation_uuid] = uuid
-                # print(conversation_uuids)
-        except:
-            pass
+            conversation_uuid = data["conversation_uuid"]
+            uuid = data["uuid"]
+            conversation_uuids[conversation_uuid].append(uuid)
+            uuids.append(uuid)
 
-
-        try:
-            if data["status"] == "completed":
-                uuids.clear()
-
-                ws_conversation_id = conversation_uuids[data["conversation_uuid"]]
-                response = client.update_call(ws_conversation_id, action='hangup')
-                conversation_uuids[data["conversation_uuid"]] = ''
-                print(response)
-
-        except Exception as e:
-            print(e)
-            pass
-
-
+        if data["status"] == "completed":
+            conversation_uuid = data["conversation_uuid"]
+            nexmo_client.hangup(conversation_uuid)
+            conversation_uuids[conversation_uuid].clear()
         self.content_type = 'text/plain'
         self.write('ok')
         self.finish()
@@ -303,7 +273,7 @@ class AcceptNumberHandler(tornado.web.RequestHandler):
               },
              {
              "action": "connect",
-              "eventUrl": [self.request.protocol +"://" + self.request.host  + "/event"],
+              # "eventUrl": [self.request.protocol +"://" + self.request.host  + "/event"],
                "from": MY_LVN,
                "endpoint": [
                  {
@@ -314,7 +284,7 @@ class AcceptNumberHandler(tornado.web.RequestHandler):
              },
               {
                  "action": "connect",
-                 "eventUrl": [self.request.protocol +"://" + self.request.host  +"/event"],
+                 # "eventUrl": [self.request.protocol +"://" + self.request.host  +"/event"],
                  "from": MY_LVN,
                  "endpoint": [
                      {
@@ -322,7 +292,7 @@ class AcceptNumberHandler(tornado.web.RequestHandler):
                         "uri" : "ws://"+self.request.host +"/socket",
                         "content-type": "audio/l16;rate=16000",
                         "headers": {
-                            "uuid":data["uuid"]
+                            "conversation_uuid":data["conversation_uuid"] #change to user
                         }
                      }
                  ]
@@ -349,8 +319,19 @@ class RecordHandler(tornado.web.RequestHandler):
         self.set_header("Content-Type", 'text/plain')
         self.finish()
 
+class PingHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.write('ok')
+        self.set_header("Content-Type", 'text/plain')
+        self.finish()
+
+
 def main():
     try:
+        global nexmo_client
+        nexmo_client = NexmoClient()
+
         logging.basicConfig(
             level=logging.INFO,
             format="%(levelname)7s %(message)s",
